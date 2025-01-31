3fb81ad1cbb89cd09bf629a9aea6836bb08dd8a2
Naresh-Cod <eie.engi.naresh@gmail.com>
2025-01-31 15:09:47 +0500

    insurance

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/forms')
+def forms():
+    return render_template('forms.html')
+
+@app.route('/model_score')
+def model_score():
+    return render_template('model_score.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
+  
