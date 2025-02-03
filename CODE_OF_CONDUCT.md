64687d26aeaae959e18a88ca697ec885e2b3efff
Naresh-Cod <eie.engi.naresh@gmail.com>
2025-02-03 19:56:10 +0500

    database creation

---
+++ CODE_OF_CONDUCT.md
@@ -0,0 +1,15 @@
+# Contributor Covenant Code of Conduct
+
+## Our Commitment
+We pledge to create a welcoming, inclusive, and respectful environment for all contributors.
+
+## Our Standards
+Examples of behavior that contributes to a positive environment:
+- Using welcoming and inclusive language.
+- Being respectful of differing viewpoints and experiences.
+- Accepting constructive criticism with grace.
+
+
+git add CODE_OF_CONDUCT.md
+git commit -m "Added Code of Conduct"
+git push origin main
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
+import pandas as pd
+import numpy as np
+import pickle
 import os
 
 app = Flask(__name__)
@@ -14,6 +17,27 @@ def forms():
 @app.route('/model_score')
 def model_score():
     return render_template('model_score.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    
+    age = request.form.get('Age')
+    sex = request.form.get('Sex')
+    bmi = request.form.get('Bmi')
+    children = request.form.get('Children')
+    smoker = request.form.get('Smoker')
+    region = request.form.get('Region')
+    data=[[age, sex, bmi, children, smoker, region]]
+
+
+    input=pd.DataFrame(data,columns=['age', 'sex', 'bmi', 'children', 'smoker', 'region'])
+    
+    with open("/Users/gauravjangid/PycharmProjects/insurance project/model/best_model.pkl","rb") as file:
+        pipeline=pickle.load(file)
+        
+    pred=pipeline.predict(input)
+    return render_template('model_score.html', pred=np.round(pred, 2))
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=1000)
